05eb5d528a8062f5ac3294d0bc28d4c1ac83ccb8
kolekardileep <dileep.kolekar@gmail.com>
2024-07-10 11:23:53 +0530

    Update BaseClass.java

--- Vtiger_Project/src/main/java/Generic_Utilities/BaseClass.java
+++ Vtiger_Project/src/main/java/Generic_Utilities/BaseClass.java
@@ -98,7 +98,6 @@ public class BaseClass {
 	public void as() throws Throwable {
 		dlib.dataBaseClose();
 		System.out.println("close database");
-		System.out.println("WELCOME TO JENKINS ON 10TH JULY 2024");
 
 	}
 }
